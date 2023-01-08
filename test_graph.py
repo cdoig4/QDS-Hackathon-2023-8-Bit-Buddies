41b546452f35300145aae48d45beda889522653e
cdoig4 <cdoig4@my.bcit.ca>
2023-01-07 21:42:18 -0800

    Create list of correlation coefficient for graph use

--- test_graph.py
+++ test_graph.py
@@ -42,9 +42,25 @@ def min_wage_data():
     plt.show()
 
 
-def calculate_correlation(x_axis_data, y_axis_data):
-    data_2018 = pd.read_csv('happiness_via_min_wage.csv')
-    x_axis_values = data_2018[x_axis_data]
-    y_axis_values = data_2018[y_axis_data]
-    correlation, _ = pearsonr(x_axis_values, y_axis_values)
-    return correlation
+# data_2019 = pd.read_csv('2019.csv')
+# happiness_score = data_2019['Happiness score']
+# gdp_per_capita = data_2019['GDP per capita']
+# social_support = data_2019['Social support']
+# life_expectancy = data_2019['Healthy life expectancy']
+# freedom = data_2019['Freedom to make life choices']
+# generosity = data_2019['Generosity']
+# corruption = data_2019['Perceptions of corruption']
+# gdp_correlation, _ = pearsonr(happiness_score, gdp_per_capita)
+# social_support_correlation, _ = pearsonr(happiness_score, social_support)
+# life_expectancy_correlation, _ = pearsonr(happiness_score, life_expectancy)
+# freedom_correlation, _ = pearsonr(happiness_score, freedom)
+# generosity_correlation, _ = pearsonr(happiness_score, generosity)
+# corruption_correlation, _ = pearsonr(happiness_score, corruption)
+#
+# correlation_list = [gdp_correlation, social_support_correlation, life_expectancy_correlation, freedom_correlation,
+#                     generosity_correlation, corruption_correlation]
+#
+# print(correlation_list)
+
+correlation_list_2019 = [[0.7938828678781273, 0.777057788063864, 0.7798831492425831, 0.56674182571999,
+                          0.07582369490389648, 0.38561307086647867]]
